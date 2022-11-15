67c2bd61fbafa2601ce682320e5076cf09b283fd
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-11-15 21:46:39 +0530

    add the styles property to the @Component decorator

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -2,7 +2,15 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'app-warning-alert',
-  templateUrl: './warning-alert.component.html'
+  templateUrl: './warning-alert.component.html',
+  styles: [`
+    p {
+      padding: 20px;
+      background-color: mistyrose;
+      border: 2px solid red;
+    }
+  `
+  ]
 })
 export class WarningAlertComponent {
 
