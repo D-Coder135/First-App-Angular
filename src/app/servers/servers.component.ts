fca335f0ae927008a2a251561c8c88a0c40e8ae6
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-11-16 16:32:01 +0530

    add some fix up to the code

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
-  serverCreationStatus = "No Server Was Created!";
+  serverCreationStatus = 'No Server Was Created!';
 
   constructor() {
     setTimeout(() => {
@@ -20,6 +20,6 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = "Server was created!";
+    this.serverCreationStatus = 'Server was created!';
   }
 }
